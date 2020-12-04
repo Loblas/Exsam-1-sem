4ea800f52c0e35cdecbca2255ffa26f7c59aa1f5
Oli <60607238+Loblas@users.noreply.github.com>
2020-12-04 13:31:52 +0100

    web and mob
    website and mobil

---
+++ java.js
@@ -0,0 +1,9 @@
+document.readyState
+
+function openAboutMe() {
+    document.getElementById("AboutMe").style.width = "50%";
+}
+
+function closeAboutMe() {
+    document.getElementById("AboutMe").style.width = "0%";
+}
