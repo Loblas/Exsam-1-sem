8ee35a80569fbfd15e1928e4674d0f04d46be2f1
Oli <60607238+Loblas@users.noreply.github.com>
2020-12-09 17:31:41 +0100

    Big changes
    Removeds java file for validation
    other smaal changes

--- java.js
+++
@@ -1,9 +0,0 @@
-document.readyState
-
-function openAboutMe() {
-    document.getElementById("AboutMe").style.width = "50%";
-}
-
-function closeAboutMe() {
-    document.getElementById("AboutMe").style.width = "0%";
-}
